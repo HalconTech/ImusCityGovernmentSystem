3f9a70793b0402a6c707b60baad1d956eb8a012d
Oliver <Oliver@DESKTOP-1GGN791>
2019-03-09 11:13:22 +0800

    Updating employee management GUI

---
+++ ImusCityGovernmentSystem/General/EmployeeModule/EmployeeModuleWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ImusCityGovernmentSystem.General.EmployeeModule
+{
+    /// <summary>
+    /// Interaction logic for EmployeeModuleWindow.xaml
+    /// </summary>
+    public partial class EmployeeModuleWindow : MetroWindow
+    {
+        public EmployeeModuleWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void employeelistbtn_Click(object sender, RoutedEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            EmployeeListWindow employee = new EmployeeListWindow();
+            Mouse.OverrideCursor = null;
+            employee.ShowDialog();
+        }
+
+        private void addempbtn_Click(object sender, RoutedEventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+            AddNewEmployeeWindow addemp = new AddNewEmployeeWindow();
+            Mouse.OverrideCursor = null;
+            addemp.ShowDialog();
+        }
+    }
+}
