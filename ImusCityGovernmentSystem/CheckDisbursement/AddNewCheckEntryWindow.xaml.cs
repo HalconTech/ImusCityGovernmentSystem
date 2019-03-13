e2125b0cbe147868a7f42a9c6be98f137aedd88f
Oliver <Oliver@DESKTOP-1GGN791>
2019-03-13 21:22:45 +0800

    Added check creation module

--- ImusCityGovernmentSystem/CheckDisbursement/AddNewCheckEntryWindow.xaml.cs
+++ ImusCityGovernmentSystem/CheckDisbursement/AddNewCheckEntryWindow.xaml.cs
@@ -28,11 +28,83 @@ namespace ImusCityGovernmentSystem.CheckDisbursement
 
         private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            if(SystemClass.CheckConnection())
+            if (SystemClass.CheckConnection())
             {
                 ImusCityHallEntities db = new ImusCityHallEntities();
                 Disbursement disbursement = db.Disbursements.Find(DisbursementID);
                 vouchernotb.Text = disbursement.VoucherNo;
+                payeetb.Text = disbursement.Payee.CompanyName;
+                descriptiontb.Text = disbursement.Description;
+                paymenttypetb.Text = disbursement.PaymentType.Name;
+
+
+                fundcb.ItemsSource = db.Funds.OrderBy(m => m.FundName).ToList();
+                fundcb.DisplayMemberPath = "FundCode";
+                fundcb.SelectedValuePath = "FundID";
+
+
+            }
+            else
+            {
+                MessageBox.Show(SystemClass.DBConnectionErrorMessage);
+            }
+        }
+
+        private void savebtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (SystemClass.CheckConnection())
+            {
+                ImusCityHallEntities db = new ImusCityHallEntities();
+                if (String.IsNullOrEmpty(checknotb.Text))
+                {
+                    MessageBox.Show("Please provide the check number");
+                }
+                else if (fundcb.SelectedValue == null)
+                {
+                    MessageBox.Show("Please select fund");
+                }
+                else if (String.IsNullOrEmpty(checkdesctb.Text))
+                {
+                    MessageBox.Show("Please provide check description");
+                }
+                else if (String.IsNullOrEmpty(checkamounttb.Text))
+                {
+                    MessageBox.Show("Please enter check amount");
+                }
+                else if(db.Checks.Any(m => m.CheckNo == checknotb.Text))
+                {
+                    MessageBox.Show("Check number is already been used");
+                }
+                else
+                {
+                   
+                    Check check = new Check();
+                    check.DisbursementID = DisbursementID;
+                    check.FundID = (int)fundcb.SelectedValue;
+                    check.CheckNo = checknotb.Text;
+                    check.CheckDescription = checkdesctb.Text;
+                    check.Amount = Convert.ToDecimal(checkamounttb.Text);
+                    check.EmployeeID = App.EmployeeID;
+                    check.DateCreated = DateTime.Now;
+                    db.Checks.Add(check);
+                    db.SaveChanges();
+
+                    var audit = new AuditTrailModel
+                    {
+                        Activity = "Created new check entry DIS ID: " + DisbursementID.ToString(),
+                        ModuleName = this.GetType().Name,
+                        EmployeeID = App.EmployeeID
+                    };
+
+                    SystemClass.InsertLog(audit);
+                    MessageBox.Show("Check created successfully!");
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show(SystemClass.DBConnectionErrorMessage);
             }
         }
     }
